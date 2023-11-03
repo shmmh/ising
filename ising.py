98883542dd1f5e25345d06a23ea5a4fd514ff158
Shammah Rashad <shammah.rashad@gmail.com>
2023-11-03 10:50:36 +0000

    moved running the model into a ipynb file

--- ising.py
+++ ising.py
@@ -63,36 +63,4 @@ class Ising:
         plt.close(fig)  # Close the figure to avoid displaying it here
         
 
-
-
-L, B, J, beta = 32, -0.05, 0.5, 30
-ising = Ising(L,J,B, beta)
-
-num_therm = 30
-
-
-mag_per_spin_arr = np.zeros(num_therm)
-visual_arr = []
-
-for i in range(num_therm):
-    mag_per_spin = ising.do_thermalisation_sweep()
-    mag_per_spin_arr[i] = mag_per_spin
-    ising.visualise(i+1)
-
-
-# plot magnetisation per spin array against numper of thermalisation sweeps
-plt.plot(mag_per_spin_arr)
-
-
-print(mag_per_spin_arr[-1])
-
-# Create a slider to select the thermalisation sweep to display
-slider = widgets.IntSlider(min=0, max=num_therm-1, step=1, value=0)
-
-# Display the slider and the corresponding thermalisation sweep
-def update_visualisation(i):
-    display(ising.visualisations[i])
-
-widgets.interactive(update_visualisation, i=slider)
-
     

ba2b5eeadee98e17d7439f0f33bcbd3305ec856b
Shammah Rashad <shammah.rashad@gmail.com>
2023-11-03 10:43:50 +0000

    renamed everything to ising from isling

--- ising.py
+++ ising.py
@@ -4,14 +4,21 @@ import ipywidgets as widgets
 from IPython.display import display
 
 
-class Isling:
-    def __init__(self, L,J,B) -> None:
+class Ising:
+    ''' A class that implements the Ising model
+    L: size of square lattice
+    J: interaction energy
+    B: strength of external magnetic field
+    beta: 1/kT Units of 1 and defaults to 1
+    
+    '''
+    def __init__(self, L,J,B, beta=1) -> None:
         self.L = L
         self.J = J
         self.B = B
         self.T = 273.15 # K
         self.k = 1.381e-23  # m2 kg s-2 K-1
-        self.beta = 1 #/ (self.k*self.T)
+        self.beta = beta #/ (self.k*self.T)
         self.lattice = 2*np.random.randint(2, size=(L,L)) - 1 #spins either -1 or 1
         self.visualisations = []
         
@@ -33,11 +40,10 @@ class Isling:
     def should_flip(self, i,j) -> bool:
         dE = self.deltaE(i,j)
         rand_num = np.random.rand()
-        prob = np.exp(-dE*self.beta)
+        prob = np.exp(-dE/self.beta)
         return prob > 1 or prob > rand_num
     
     def do_thermalisation_sweep(self):
-        
         for i in range(self.L):
             for j in range(self.L):
                 self.flip_spin(i,j)
@@ -51,7 +57,7 @@ class Isling:
     
     def visualise(self, i):
         fig, ax = plt.subplots()  # Create a new figure and axes
-        ax.imshow(self.lattice, cmap='gray', vmin=-1, vmax=1)
+        ax.imshow(self.lattice, vmin=-1, vmax=1)
         ax.set_title(f'Thermalisation Sweep: {i}')
         self.visualisations.append(fig)  # Save the figure in the list
         plt.close(fig)  # Close the figure to avoid displaying it here
@@ -59,8 +65,8 @@ class Isling:
 
 
 
-L, B, J = 100, -0.05, 0.5
-isling = Isling(L,J,B)
+L, B, J, beta = 32, -0.05, 0.5, 30
+ising = Ising(L,J,B, beta)
 
 num_therm = 30
 
@@ -69,9 +75,9 @@ mag_per_spin_arr = np.zeros(num_therm)
 visual_arr = []
 
 for i in range(num_therm):
-    mag_per_spin = isling.do_thermalisation_sweep()
+    mag_per_spin = ising.do_thermalisation_sweep()
     mag_per_spin_arr[i] = mag_per_spin
-    isling.visualise(i+1)
+    ising.visualise(i+1)
 
 
 # plot magnetisation per spin array against numper of thermalisation sweeps
@@ -85,7 +91,7 @@ slider = widgets.IntSlider(min=0, max=num_therm-1, step=1, value=0)
 
 # Display the slider and the corresponding thermalisation sweep
 def update_visualisation(i):
-    display(isling.visualisations[i])
+    display(ising.visualisations[i])
 
 widgets.interactive(update_visualisation, i=slider)
 
